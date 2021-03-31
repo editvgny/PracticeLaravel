c1f7ab066592005ee1e9d26826cffe77a95ecc14
editvgny <edit.vgny@gmail.com>
2021-03-31 14:36:24 +0200

    created Post.php model with likedBy, user, likes methods

---
+++ app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'body',
+        'user_id',
+    ];
+
+    public function likedBy(User $user) {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
+//    public function ownedBy(User $user): bool {
+//        return $user->id === $this->user_id;
+//    }
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function likes(): HasMany {
+        return $this->hasMany(Like::class);
+    }
+}
